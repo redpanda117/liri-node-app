a5c8b9158388a4f62477fd88dbca0a25a014d1fe
Tina Nguyen <nguyentina2005@yahoo.com>
2017-07-10 19:35:04 -0400

    call movie info

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  
+  consumer_key: YMqC60jVzxwqNtsdeyTIE8lCf,
+  
+  consumer_secret: 	rON0mROVqFh1krjWx5UB5tCQ0FjFY5UD6nIuKm8SUZTqI6Tfom,
+  
+  access_token_key: 
+  884528863837126656-2G8NQc3ab9of9kqlTmfMURteD2jXo5t,
+  
+  access_token_secret: 	9QBVAcZ8fleDFOrvcXYeJolp3AtISm5lIPGB7cssBGEWL,
+}
---
+++ liri.js
@@ -0,0 +1,65 @@
+// Load the fs package to read and write
+var fs = require("fs");
+
+//var keys = require("./keys.js");
+//var twitter = keys.twitterKeys;
+
+var command = process.argv[2];
+var input = process.argv[3];
+
+// The switch-case will direct which function gets run depending on which command the user gives.
+
+switch (command) {
+    case "my-tweets":
+        tweets();
+        break;
+
+    case "spotify-this-song":
+        spotify();
+        break;
+
+    case "movie-this":
+        movie();
+        break;
+
+    case "do-what-it-says":
+        doIt();
+        break;
+        
+    default:
+        console.log("Not a Choice");
+        break;
+};
+
+function movie() {
+// Then run a request to the OMDB API with the movie specified
+    var arr = process.argv.slice(3);
+    var movieName = arr.join('+');
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    
+    var request = require("request");
+request(queryUrl, function(error, response, body) {
+      // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {   
+//console.log(body);// to find object year name
+
+// showing the desire information in terminal
+      console.log(JSON.parse(body).Title);
+      
+      console.log("This movie came out in " +  JSON.parse(body).Year);
+      
+      console.log("IMDB rated the movie: " + JSON.parse(body).imdbRating);
+      
+      console.log("Rotten Tomatoes score: " + JSON.parse(body).Ratings[1].Value);
+      
+      console.log("Movie was produced in " + JSON.parse(body).Country)
+
+      console.log("Language: " + JSON.parse(body).Language);
+      
+      console.log("Plot: " + JSON.parse(body).Plot);
+      
+      console.log("Staring: " + JSON.parse(body).Actors);
+  }
+});
+}
